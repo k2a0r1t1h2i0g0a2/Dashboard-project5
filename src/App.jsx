e39357af7e18e0f3335b82cb9eafdcbc6edbe9b9
k2a0r1t1h2i0g0a2 <karthiga.r2002@gmail.com>
2023-09-03 12:37:00 +0530

    files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from './assets/components/Grid';
+
+const App = () => {
+  return (
+    <div>
+      <Grid />
+      
+    </div>
+  );
+};
+
+export default App;
